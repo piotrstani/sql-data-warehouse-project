c687e5dc492aa634068546641484cdda077b50cc
piotrstani <piotr1670@pm.me>
2026-09-04 20:45:33 +0200

    refactor postgres

--- scripts/create_database.sql
+++ scripts/create_database.sql
@@ -1,13 +1,8 @@
 -- Usuń bazę, jeśli istnieje
-DROP DATABASE IF EXISTS "training_db";
+DROP DATABASE IF EXISTS "DataWarehouse";
 
 -- Utwórz bazę
 CREATE DATABASE "DataWarehouse";
 
 -- Sprawdz bazy
 SELECT datname FROM pg_database;
-
-
-CREATE SCHEMA bronze;
-CREATE SCHEMA silver;
-CREATE SCHEMA gold;
