ccc7fe168acffbf9df6e01122ede1733d0c32e6c
piotrstani <piotr1670@pm.me>
2026-09-13 19:14:04 +0200

    airflow dag dla GE

--- dags/load_bronze_dag.py
+++ dags/load_bronze_dag.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator # Nowy import
 
 # Domyślne argumenty dla zadań w DAG-u
 default_args = {
@@ -16,17 +17,24 @@ with DAG(
     start_date=datetime(2024, 1, 1),
     schedule_interval='@daily', # Uruchamiaj raz dziennie
     catchup=False,              # Nie nadrabiaj zaległych uruchomień z przeszłości
-    tags=['bronze', 'ingestion'],
-    description='Ładuje surowe pliki CSV do warstwy Bronze w PostgreSQL'
+    tags=['bronze', 'ingestion', 'dq'],
+    description='Ładuje surowe pliki CSV do warstwy Bronze w PostgreSQ, wykonuje prosty test GE'
 ) as dag:
 
+    # 1. Bronze ingestion
     # Zadanie wywołujące Twoją procedurę PL/pgSQL
-    load_bronze_procedure = SQLExecuteQueryOperator(
+        load_bronze_procedure = SQLExecuteQueryOperator(
         task_id='call_load_bronze_procedure',
         conn_id='postgres_dwh',  # Zbieżne z nazwą połączenia w Airflow
         sql="CALL bronze.load_bronze();",
         autocommit=True          # Wymagane dla niektórych operacji proceduralnych
     )
+    # 2. Test Great Expectations
+        run_ge_bronze_tests = BashOperator(
+        task_id='run_great_expectations_bronze',
+        # Uruchamiamy z głównego katalogu kontenera, gdzie znajduje się plik .env
+        bash_command='cd /opt/airflow && python tests/great_expectations/ge_tests_bronze.py'
+        )
 
-    # Tutaj w przyszłości dodasz kolejne zadania (np. dbt, Great Expectations)
-    # load_bronze_procedure >> dbt_run_silver
+        # Ustalenie kolejności wykonywania zadań
+        load_bronze_procedure >> run_ge_bronze_tests
