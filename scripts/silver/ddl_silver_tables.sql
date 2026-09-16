cbc2951283e4f12113c6f50fa40b354a9489fbab
piotrstani <piotr1670@pm.me>
2026-09-16 15:32:57 +0200

    silver ddl

---
+++ scripts/silver/ddl_silver_tables.sql
@@ -0,0 +1,75 @@
+/*
+===============================================================================
+    This DDL script creates tables in the 'silver' schema, dropping existing tables
+    if they already exist.
+===============================================================================
+*/
+
+-------------------------------------------------------------------------------1
+DROP TABLE IF EXISTS silver.crm_cust_info;
+CREATE TABLE IF NOT EXISTS silver.crm_cust_info (
+    cst_id 				integer,
+    cst_key             VARCHAR(50),
+    cst_firstname       VARCHAR(50),
+    cst_lastname        VARCHAR(50),
+    cst_marital_status  VARCHAR(50),
+    cst_gndr            VARCHAR(50),
+    cst_create_date     DATE,
+    dwh_create_ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-------------------------------------------------------------------------------2
+DROP TABLE IF EXISTS silver.crm_prd_info;
+CREATE TABLE IF NOT EXISTS silver.crm_prd_info (
+    prd_id       INT,
+    prd_key      VARCHAR(50),
+    prd_nm       VARCHAR(50),
+    prd_cost     INT,
+    prd_line     VARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt   DATE,
+    dwh_create_ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-------------------------------------------------------------------------------3
+DROP TABLE IF EXISTS silver.crm_sales_details;
+CREATE TABLE IF NOT EXISTS silver.crm_sales_details (
+    sls_ord_num  VARCHAR(50),
+    sls_prd_key  VARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT,
+    dwh_create_ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-------------------------------------------------------------------------------4
+DROP TABLE IF EXISTS silver.erp_loc_a101;
+CREATE TABLE IF NOT EXISTS silver.erp_loc_a101 (
+    cid    VARCHAR(50),
+    cntry  VARCHAR(50),
+    dwh_create_ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-------------------------------------------------------------------------------5
+DROP TABLE IF EXISTS silver.erp_cust_az12;
+CREATE TABLE IF NOT EXISTS silver.erp_cust_az12 (
+    cid    VARCHAR(50),
+    bdate  DATE,
+    gen    VARCHAR(50),
+    dwh_create_ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-------------------------------------------------------------------------------6
+DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
+CREATE TABLE IF NOT EXISTS silver.erp_px_cat_g1v2 (
+    id           VARCHAR(50),
+    cat          VARCHAR(50),
+    subcat       VARCHAR(50),
+    maintenance  VARCHAR(50),
+    dwh_create_ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
