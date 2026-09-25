276c3c042635fc15118f8c936796e8fb690e82bc
piotrstani <piotr1670@pm.me>
2026-09-25 14:07:30 +0200

    gold sql products

--- scripts/gold/load_gold_layer.sql
+++ scripts/gold/load_gold_layer.sql
@@ -13,9 +13,32 @@ select
 	eca.bdate as birthdate,	
 	ela.cntry as country,
 	cci.cst_create_date as create_date
-from silver.crm_cust_info cci
-left join silver.erp_cust_az12 eca 
+from silver.crm_cust_info as cci
+left join silver.erp_cust_az12 asc eca 
 	on cci.cst_key = eca.cid 
 left join silver.erp_loc_a101 ela 
-	on cci.cst_key = ela.cid 
+	on cci.cst_key = ela.cid;
+
 --------------------------------------------------------------PRODUCT
+select
+	MD5(cpi.prd_key::text) AS product_key,
+	cpi.prd_id as product_id,
+	cpi.prd_key as product_number,
+	cpi.prd_nm as product_name,
+	cpi.cat_id as category_id,
+	epcgv.cat as category,
+	epcgv.subcat as subcategory,
+	epcgv.maintenance as maintenance,
+	cpi.prd_cost as cost,
+	cpi.prd_line as product_line,
+	cpi.prd_start_dt as start_date
+from silver.crm_prd_info as cpi
+left join silver.erp_px_cat_g1v2 as epcgv 
+on cpi.cat_id=epcgv.id 
+where 
+cpi.prd_end_dt is null --only active
+
+
+
+
+
