ced07e86864d74108e7e44c138212f4e7452258c
piotrstani <piotr1670@pm.me>
2026-09-25 13:52:47 +0200

    gold sql customers

--- scripts/gold/load_gold_layer.sql
+++ scripts/gold/load_gold_layer.sql
@@ -1,19 +1,21 @@
 --------------------------------------------------------------CUSTOMER
+--CRM is master 
+create view gold.dim_customers as
 select
-	cci.cst_id,
-	cci.cst_key,
-	cci.cst_firstname,
-	cci.cst_lastname,
-	cci.cst_marital_status,
-	case when cci.cst_gndr != 'n/a' then cci.cst_gndr
-	else coalesce (eca.gen, 'n/a') end as new_gen,
-	cci.cst_create_date,
-	eca.bdate,	
-	ela.cntry 
+	MD5(cci.cst_id::text) AS customer_key,
+	cci.cst_id as customer_id,
+	cci.cst_key as customer_number,
+	cci.cst_firstname as first_name,
+	cci.cst_lastname as last_name,
+	cci.cst_marital_status as martial_status,
+	case when cci.cst_gndr != 'n/a' then cci.cst_gndr 
+		else coalesce (eca.gen, 'n/a') end as gender,
+	eca.bdate as birthdate,	
+	ela.cntry as country,
+	cci.cst_create_date as create_date
 from silver.crm_cust_info cci
 left join silver.erp_cust_az12 eca 
 	on cci.cst_key = eca.cid 
 left join silver.erp_loc_a101 ela 
 	on cci.cst_key = ela.cid 
-	
 --------------------------------------------------------------PRODUCT
