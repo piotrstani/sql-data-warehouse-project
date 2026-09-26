8c7b83d4860311c1f088482583b11ecbb4e0c47b
piotrstani <piotr1670@pm.me>
2026-09-26 18:26:42 +0200

    sql gold fixes

--- scripts/gold/load_gold_layer.sql
+++ scripts/gold/load_gold_layer.sql
@@ -1,6 +1,6 @@
 --------------------------------------------------------------CUSTOMER
 --CRM is master 
-create view .dim_customers as
+create view gold.dim_customers as
 select
 	MD5(cci.cst_id::text) AS customer_key,
 	cci.cst_id as customer_id,
@@ -24,14 +24,14 @@ COMMENT ON VIEW gold.dim_customers IS 'Stores customer details enriched with dem
 
 --Description columns
 COMMENT ON COLUMN gold.dim_customers.customer_key IS 'Surrogate key uniquely identifying each customer record in the dimension table';
-COMMENT ON COLUMN gold.dim_customers.customer_id IS 'Unique numerical identifier assigned to each customer'
-COMMENT ON COLUMN gold.dim_customers.customer_number IS 'Alphanumeric identifier representing the customer, used for tracking and referencing'
+COMMENT ON COLUMN gold.dim_customers.customer_id IS 'Unique numerical identifier assigned to each customer';
+COMMENT ON COLUMN gold.dim_customers.customer_number IS 'Alphanumeric identifier representing the customer, used for tracking and referencing';
 COMMENT ON COLUMN gold.dim_customers.first_name IS 'The customer''s first name, as recorded in the system';
 COMMENT ON COLUMN gold.dim_customers.last_name IS 'The customer''s last name or family name';
 COMMENT ON COLUMN gold.dim_customers.country IS 'The country of residence for the customer';
 COMMENT ON COLUMN gold.dim_customers.martial_status IS 'The marital status of the customer';
 COMMENT ON COLUMN gold.dim_customers.gender IS 'The gender of the customer';
-COMMENT ON COLUMN gold.dim_customers.birthdate IS 'The date of birth of the customer, formatted as YYYY-MM-DD'
+COMMENT ON COLUMN gold.dim_customers.birthdate IS 'The date of birth of the customer, formatted as YYYY-MM-DD';
 COMMENT ON COLUMN gold.dim_customers.create_date IS 'The date and time when the customer record was created in the system';
 
 
@@ -59,14 +59,14 @@ COMMENT ON VIEW gold.dim_products IS 'Provides information about the products an
 
 --Description columns
 COMMENT ON COLUMN gold.dim_products.product_key IS 'Surrogate key uniquely identifying each product record in the product dimension table';
-COMMENT ON COLUMN gold.dim_products.product_id IS 'A unique identifier assigned to the product for internal tracking and referencing'
-COMMENT ON COLUMN gold.dim_products.product_number IS 'A structured alphanumeric code representing the product, often used for categorization or inventor'
+COMMENT ON COLUMN gold.dim_products.product_id IS 'A unique identifier assigned to the product for internal tracking and referencing';
+COMMENT ON COLUMN gold.dim_products.product_number IS 'A structured alphanumeric code representing the product, often used for categorization or inventor';
 COMMENT ON COLUMN gold.dim_products.product_name IS 'Descriptive name of the product, including key details such as type, color, and size';
 COMMENT ON COLUMN gold.dim_products.category_id IS 'A unique identifier for the product''s category, linking to its high-level classification';
 COMMENT ON COLUMN gold.dim_products.category IS 'The broader classification of the product (e.g., Bikes, Components) to group related items';
 COMMENT ON COLUMN gold.dim_products.subcategory IS 'A more detailed classification of the product within the category, such as product type';
 COMMENT ON COLUMN gold.dim_products.maintenance IS 'Indicates whether the product requires maintenance';
-COMMENT ON COLUMN gold.dim_products.cost IS 'The cost or base price of the product, measured in monetary units'
+COMMENT ON COLUMN gold.dim_products.cost IS 'The cost or base price of the product, measured in monetary units';
 COMMENT ON COLUMN gold.dim_products.product_line IS 'The specific product line or series to which the product belongs';
 COMMENT ON COLUMN gold.dim_products.start_date IS 'The date when the product became available for sale or use, stored in';
 
@@ -88,28 +88,28 @@ left join gold.dim_products as dp
 left join gold.dim_customers  as dc 
 	on csd.sls_cust_id = dc.customer_id;
 
----test
-select * from gold.fact_sales t 
-left join gold.dim_customers dc 
-on t.customer_key =dc.customer_key 
-left join gold.dim_products dp 
-on t.product_key =dp.product_key 
---where dc.customer_key is null 
-where dp.product_key  is null 
-
-
 --Description
 COMMENT ON VIEW gold.fact_sales IS 'Stores transactional sales data for analytical purposes';
 
 --Description columns
 COMMENT ON COLUMN gold.fact_sales.order_number IS 'A unique alphanumeric identifier for each sales order';
-COMMENT ON COLUMN gold.fact_sales.product_key IS 'Surrogate key linking the order to the product dimension table'
-COMMENT ON COLUMN gold.fact_sales.customer_key IS 'Surrogate key linking the order to the customer dimension table'
+COMMENT ON COLUMN gold.fact_sales.product_key IS 'Surrogate key linking the order to the product dimension table';
+COMMENT ON COLUMN gold.fact_sales.customer_key IS 'Surrogate key linking the order to the customer dimension table';
 COMMENT ON COLUMN gold.fact_sales.order_date IS 'The date when the order was placed';
 COMMENT ON COLUMN gold.fact_sales.shipping_date IS 'The date when the order was shipped to the customer';
 COMMENT ON COLUMN gold.fact_sales.due_dt IS 'The date when the order payment was due';
 COMMENT ON COLUMN gold.fact_sales.sales_amount IS 'The total monetary value of the sale for the line item, in whole currency units';
 COMMENT ON COLUMN gold.fact_sales.quantity IS 'The number of units of the product ordered for the line item';
 COMMENT ON COLUMN gold.fact_sales.price IS 'The price per unit of the product for the line item, in whole currency units';
+
+
+---test
+select * from gold.fact_sales t
+left join gold.dim_customers dc
+on t.customer_key =dc.customer_key
+left join gold.dim_products dp
+on t.product_key =dp.product_key
+--where dc.customer_key is null
+where dp.product_key  is null;
 
 
